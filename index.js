a4f65b3ccd5faa0320169d34ae1d1e0d6795c1d8
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-14 22:15:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNum) {
+  return streetNum > 42 ? streetNum - 42 : 42 - streetNum;
+}
+
+function distanceFromHqInFeet(streetNum) {
+  return distanceFromHqInBlocks(streetNum)*264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  return Math.abs(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400){
+    return 0;
+  } else if (distance > 400 && distance <= 2000){
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else if (distance > 2500){
+    return "cannot travel that far";
+  }
+}
